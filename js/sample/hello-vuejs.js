f25dfe8e6d39666793642ac707f818ec58ac1ff1
hainet <20400616+hainet@users.noreply.github.com>
2017-08-09 15:59:56 +0900

    ディレクティブに関するサンプルを分離

--- js/sample/hello-vuejs.js
+++ js/sample/hello-vuejs.js
@@ -20,49 +20,6 @@ var objectValue = new Vue({
     }
 })
 
-var vBind = new Vue({
-    el: '#v-bind',
-    data: {
-        message: new Date()
-    }
-})
-
-var vIf = new Vue({
-    el: '#v-if',
-    data: {
-        seen: true
-    }
-})
-
-var vFor = new Vue({
-    el: '#v-for',
-    data: {
-        skills: [
-            { name: 'Javascript' },
-            { name: 'Vue.js' }
-        ]
-    }
-})
-
-var vOn = new Vue({
-    el: '#v-on',
-    data: {
-        message: '0123456789'
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-
-var vModel = new Vue({
-    el: '#v-model',
-    data: {
-        message: 'Edit me!'
-    }
-})
-
 Vue.component('component', {
     template: '<span>template</span>'
 })
