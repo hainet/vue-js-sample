52d4b59b2921c1a096546a8326bc5ce5d65d9570
hainet <20400616+hainet@users.noreply.github.com>
2017-08-09 18:13:24 +0900

    ディレクティブのサンプルを追加

--- js/sample/directive.js
+++ js/sample/directive.js
@@ -1,7 +1,35 @@
-var vBind = new Vue({
-    el: '#v-bind',
+var vOnce = new Vue({
+    el: '#v-once',
+    data: {
+        message: 'message'
+    }
+})
+
+var vBindId = new Vue({
+    el: '#v-bind-id',
+    data: {
+        id: '1'
+    }
+})
+
+var vBindTitle = new Vue({
+    el: '#v-bind-title',
     data: {
         message: new Date()
+    }
+})
+
+var vBindHref = new Vue({
+    el: '#v-bind-href',
+    data: {
+        url: 'https://jp.vuejs.org'
+    }
+})
+
+var vBindDisabled = new Vue({
+    el: '#v-bind-disabled',
+    data: {
+        disabled: false
     }
 })
 
@@ -38,5 +66,12 @@ var vModel = new Vue({
     el: '#v-model',
     data: {
         message: 'Edit me!'
+    }
+})
+
+var vHtml = new Vue({
+    el: '#v-html',
+    data: {
+        rawHtml: '<b>This is raw html!</b>'
     }
 })
